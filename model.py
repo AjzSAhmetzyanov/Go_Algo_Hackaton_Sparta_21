bde3efc446299708743843e9817a322f9b3682e8
ayzat <akhmetzyanov0001@mail.ru>
2023-12-10 08:47:25 +0300

    fix model.py

--- model.py
+++ model.py
@@ -38,91 +38,62 @@ company_to_save = ["sber", "ozon", "tatneft", "yandex", "gazprom"]
 
 columns = ['Stock', 'Date', 'Predicted_Close', 'Signal_rol']
 
-result_dataframe = pd.DataFrame(columns=columns)
 
-for i in range(0, len(company)):
-  # Сборка данных со всех датафреймов
-  trade = company[i].tradestats(date=f'2023-12-0{day-1}', till_date=f'2023-12-0{day}')
+# Сборка данных со всех датафреймов
+def create_dataset(i):
+  trade = company[i].tradestats(date=f'2023-12-0{day-3}', till_date=f'2023-12-0{day-1}')
   trade.rename(columns={'systime':'systime_trade'}, inplace=True)
-  order = company[i].orderstats(date=f'2023-12-0{day-1}', till_date=f'2023-12-0{day}')
+  order = company[i].orderstats(date=f'2023-12-0{day-3}', till_date=f'2023-12-0{day-1}')
   order.rename(columns={'systime':'systime_order'}, inplace=True)
-  obs = company[i].obstats(date=f'2023-12-0{day-1}', till_date=f'2023-12-0{day}')
+  obs = company[i].obstats(date=f'2023-12-0{day-3}', till_date=f'2023-12-0{day-1}')
   obs.rename(columns={'val_b':'val_b_obs', 'val_s':'val_s_obs', 'vol_b':'vol_b_obs', 'vol_s':'vol_s_obs',
-                      'systime':'systime_obs'}, inplace=True)
+                        'systime':'systime_obs'}, inplace=True)
 
   data = pd.merge(trade, order, on=['ticker','tradedate', 'tradetime'])
   data = pd.merge(data, obs, on=['ticker','tradedate', 'tradetime'])
+  return data
 
-  # Оценка важности признаков
-  # RandomForestRegressor metod
-  exclude_columns = ['ticker', 'tradedate','tradetime', 'systime_trade',
-                    'systime_order', 'systime_obs']
-  data_ = data.copy()
-  data_['tradedate'] = pd.to_datetime(data_['tradedate'])
-  data_['tradetime'] = pd.to_datetime(data_['tradetime'], format='%H:%M:%S').dt.time
-  data_['datetime'] = pd.to_datetime(data_['tradedate'].astype(str) + ' ' + data_['tradetime'].astype(str))
-  data_['datetime'] = data_['datetime'] + pd.to_timedelta('7 days')
-  data_ = data_.drop(columns=['tradedate', 'tradetime'])
-  data = data.drop(columns=exclude_columns)
-  target_column = 'pr_close'
-  X = data.drop(columns=[target_column]).fillna(0)
-  y = data[target_column]
+# Создание последовательности данных для LSTM
+def create_sequences(data_, sequence_length):
+    sequences = []
+    targets = []
+    for j in range(len(data_) - sequence_length):
+        sequences.append(data_[j:j+sequence_length])
+        targets.append(data_[j+sequence_length, 0])
+    return np.array(sequences), np.array(targets)
+
+# RandomForestRegressor metod
+def random_forest_method(X, y):  
   rf_model = RandomForestRegressor(random_state=42)
   rf_model.fit(X, y)
-  feature_importance_df = rf_model.feature_importances_
+  feature_importances = rf_model.feature_importances_
   feature_importance_df = pd.DataFrame({'Feature': X.columns, 'Importance': feature_importances})
   feature_importance_df = feature_importance_df.sort_values(by='Importance', ascending=False)
+  return feature_importance_df, rf_model
 
-  # Метод одномерного анализа вариации (ANOVA):
+# Метод одномерного анализа вариации (ANOVA):
+def anova_method(X, y):
   f_scores, p_values = f_classif(X, y)
   anova_importance_df = pd.DataFrame({'Feature': X.columns, 'F-Score': f_scores, 'P-Value': p_values})
   anova_importance_df = anova_importance_df.sort_values(by='F-Score', ascending=False)
+  return anova_importance_df
 
-  # Метод взаимной информации:
+# Метод взаимной информации:
+def mi_method(X, y):
   mi_scores = mutual_info_regression(X, y)
   mi_importance_df = pd.DataFrame({'Feature': X.columns, 'MI Score': mi_scores})
   mi_importance_df = mi_importance_df.sort_values(by='MI Score', ascending=False)
+  return mi_importance_df
 
-  # Пермутационные важности:
+# Пермутационные важности:
+def perm_method(X, y, rf_model):
   perm_importance = permutation_importance(rf_model, X, y, n_repeats=30, random_state=42)
   perm_importance_df = pd.DataFrame({'Feature': X.columns, 'Importance': perm_importance.importances_mean})
   perm_importance_df = perm_importance_df.sort_values(by='Importance', ascending=False)
+  return perm_importance_df
 
-  # Отбираем наиболее значимые признаки
-  fi = list(feature_importance_df[0:10]['Feature'].values)
-  ai = list(anova_importance_df[0:10]['Feature'].values)
-  mi = list(mi_importance_df[0:10]['Feature'].values)
-  pi = list(perm_importance_df[0:10]['Feature'].values)
-
-  result = list(set(fi) & set(ai) & set(mi) & set(pi))
-
-  # Передаем их в модель для предсказывания
-
-  # Создание целевой переменной (target) - прогноз цены закрытия на следующем временном интервале
-  data_['target'] = data_['pr_close'].shift(-1)
-
-  # Удаление последней строки, так как у нее нет целевой переменной
-  candles_data = data_[:-1]
-
-  # Выбор признаков (features)
-  features = candles_data[result]
-
-  # Нормализация данных
-  scaler = MinMaxScaler()
-  scaled_features = scaler.fit_transform(features)
-
-  # Создание последовательности данных для LSTM
-  def create_sequences(data_, sequence_length):
-      sequences = []
-      targets = []
-      for j in range(len(data_) - sequence_length):
-          sequences.append(data_[j:j+sequence_length])
-          targets.append(data_[j+sequence_length, 0])  # Используем только первый столбец
-      return np.array(sequences), np.array(targets)
-
-  sequence_length = 10  # длина последовательности
-  X, y = create_sequences(scaled_features, sequence_length)
-
+# Модель:
+def model(X, y, data_, result, sequence_length, scaler):
   # Разделение данных на обучающий и тестовый наборы
   X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
@@ -156,18 +127,33 @@ for i in range(0, len(company)):
       'Date': data_.index[:len(scaled_inv)],
       'Predicted_Close': scaled_inv[:, 0]
   })
+  return future_predictions_df
 
-  # Применение стратегии скользящих средних:
-  future_predictions_df['short_ma'] = future_predictions_df['Predicted_Close'].rolling(window=20).mean()
-  future_predictions_df['long_ma'] = future_predictions_df['Predicted_Close'].rolling(window=50).mean()
+# Получение 30 значенний:
+def get_data(future_predictions_df):
+  # Индексы, где Signal_rol равен 1 или -1
+  indexes_1 = future_predictions_df[future_predictions_df['Signal_rol'] == 1].index
+  indexes_minus_1 = future_predictions_df[future_predictions_df['Signal_rol'] == -1].index
 
-  # Сигналы покупки и продажи
-  future_predictions_df["Signal_rol"] = 0
-  future_predictions_df.loc[future_predictions_df['short_ma'] > future_predictions_df['long_ma'], 'Signal_rol'] = 1
-  future_predictions_df.loc[future_predictions_df['short_ma'] < future_predictions_df['long_ma'], 'Signal_rol'] = -1
+  # Всего 30 значений
+  total_values = 30
 
-  future_predictions_df['Previous_Signal_Rol'] = future_predictions_df['Signal_rol'].shift(1)
+  # Равные интервалы для оставшихся значений
+  remaining_indexes = []
+  step = len(future_predictions_df) // (total_values - 2)
+  for k in range(0, len(future_predictions_df), step):
+      remaining_indexes.append(future_predictions_df.index[k])
 
+  # Объединение всех индексов и выбор уникальных
+  selected_indexes = set(indexes_1) | set(indexes_minus_1) | set(remaining_indexes)
+
+  # print(future_predictions_df)
+  # Получение итогового DataFrame
+  result_data = future_predictions_df.loc[sorted(list(selected_indexes))]
+  return result_data
+
+# Ищем даты для покупки и продажи
+def get_bidding_date(future_predictions_df):
   # Создаем все возможные пары сигналов
   all_signal_pairs = pd.merge(future_predictions_df, future_predictions_df, how='cross')
 
@@ -185,33 +171,108 @@ for i in range(0, len(company)):
   exit_date = best_trade['Date_y']
 
   future_predictions_df['Signal_rol'] = 0
-
-  # Обновление столбца Signal_Rol
-  future_predictions_df.loc[future_predictions_df['Date'].isin([entry_date, exit_date]), 'Signal_rol'] = [1, -1]
-
-  future_predictions_df = future_predictions_df.drop(columns=['short_ma', 'long_ma', 'Previous_Signal_Rol'])
-
-  # Индексы, где Signal_rol равен 1 или -1
-  indexes_1 = future_predictions_df[future_predictions_df['Signal_rol'] == 1].index
-  indexes_minus_1 = future_predictions_df[future_predictions_df['Signal_rol'] == -1].index
-
-  # Всего 30 значений
-  total_values = 30
-
-  # Равные интервалы для оставшихся значений
-  remaining_indexes = []
-  step = len(future_predictions_df) // (total_values - 2)
-  for k in range(0, len(future_predictions_df), step):
-      remaining_indexes.append(future_predictions_df.index[k])
-
-  # Объединение всех индексов и выбор уникальных
-  selected_indexes = set(indexes_1) | set(indexes_minus_1) | set(remaining_indexes)
   
-  # print(future_predictions_df)
-  # Получение итогового DataFrame
-  result_data = future_predictions_df.loc[sorted(list(selected_indexes))]
+  return entry_date, exit_date
 
-  result_dataframe = pd.concat([result_dataframe, result_data], ignore_index=True)
+# Применение стратегии скользящих средних:
+def strategy(future_predictions_df):
+    future_predictions_df['short_ma'] = future_predictions_df['Predicted_Close'].rolling(window=20).mean()
+    future_predictions_df['long_ma'] = future_predictions_df['Predicted_Close'].rolling(window=50).mean()
 
-result_dataframe.to_csv("result_day.csv")
-# result_dataframe
+    # Сигналы покупки и продажи
+    future_predictions_df["Signal_rol"] = 0
+    future_predictions_df.loc[future_predictions_df['short_ma'] > future_predictions_df['long_ma'], 'Signal_rol'] = 1
+    future_predictions_df.loc[future_predictions_df['short_ma'] < future_predictions_df['long_ma'], 'Signal_rol'] = -1
+
+    future_predictions_df['Previous_Signal_Rol'] = future_predictions_df['Signal_rol'].shift(1)
+    return future_predictions_df
+
+def start_model():
+
+  result_dataframe = pd.DataFrame(columns=columns)
+
+  for i in range(0, 1):
+
+    data = create_dataset(i)
+
+    exclude_columns = ['ticker', 'tradedate','tradetime', 'systime_trade',
+                      'systime_order', 'systime_obs']
+    data_ = data.copy()
+    data_['tradedate'] = pd.to_datetime(data_['tradedate'])
+    data_['tradetime'] = pd.to_datetime(data_['tradetime'], format='%H:%M:%S').dt.time
+    data_['datetime'] = pd.to_datetime(data_['tradedate'].astype(str) + ' ' + data_['tradetime'].astype(str))
+    data_['datetime'] = data_['datetime'] + pd.to_timedelta('7 days')
+    data_ = data_.drop(columns=['tradedate', 'tradetime'])
+    data = data.drop(columns=exclude_columns)
+    target_column = 'pr_close'
+
+    X = data.drop(columns=[target_column]).fillna(0)
+    y = data[target_column]
+
+    # Оценка важности признаков
+    # RandomForestRegressor metod
+    feature_importance_df = random_forest_method(X, y)[0]
+
+    # Метод одномерного анализа вариации (ANOVA):
+    anova_importance_df = anova_method(X, y)
+
+    # Метод взаимной информации:
+    mi_importance_df = mi_method(X, y)
+
+    # Пермутационные важности:
+    perm_importance_df = perm_method(X, y, random_forest_method(X,y)[1])
+
+    # Отбираем наиболее значимые признаки
+    fi = list(feature_importance_df[0:10]['Feature'].values)
+    ai = list(anova_importance_df[0:10]['Feature'].values)
+    mi = list(mi_importance_df[0:10]['Feature'].values)
+    pi = list(perm_importance_df[0:10]['Feature'].values)
+
+    result = list(set(fi) & set(ai) & set(mi) & set(pi))
+
+    # Передаем их в модель для предсказывания
+
+    # Создание целевой переменной (target) - прогноз цены закрытия на следующем временном интервале
+    data_['target'] = data_['pr_close'].shift(-1)
+
+    # Удаление последней строки, так как у нее нет целевой переменной
+    candles_data = data_[:-1]
+
+    # Выбор признаков (features)
+    features = candles_data[result]
+
+    # Нормализация данных
+    scaler = MinMaxScaler()
+    scaled_features = scaler.fit_transform(features)
+
+    sequence_length = 10  # длина последовательности
+    X, y = create_sequences(scaled_features, sequence_length)
+
+    # Применение модели
+    future_predictions_df = model(X, y, data_, result, sequence_length, scaler)
+
+    # Применение стратегии
+    future_predictions_df = strategy(future_predictions_df)
+
+    # Находим точки покупки и продажи
+    entry_date, exit_date = get_bidding_date(future_predictions_df)
+
+    # Обновление столбца Signal_Rol
+    future_predictions_df.loc[future_predictions_df['Date'].isin([entry_date, exit_date]), 'Signal_rol'] = [1, -1]
+
+    future_predictions_df = future_predictions_df.drop(columns=['short_ma', 'long_ma', 'Previous_Signal_Rol'])
+
+    result_data = get_data(future_predictions_df)
+
+    result_dataframe = pd.concat([result_dataframe, result_data], ignore_index=True)
+
+  result_dataframe.to_csv("result_day.csv")
+
+def main():
+  start_model()
+
+if __name__ == "__main__":
+  before = time.time()
+  main()
+  after = time.time()
+  print(f"{(after-before)/60} min")
